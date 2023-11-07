7ae36c2ef894b0b8f6d5da9404108519195bc496
Wilson Lim Setiawan <wilsonlimsetiawan@gmail.com>
2023-11-06 18:40:43 -0800

    new prompt

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# [coffeechatai](https://www.coffeechatai.com/)
+# [Coffee Chat AI](https://www.coffeechatai.com/)
 
 This project generates questions to ask on a Coffee Chat for you using AI.
 
 ## How it works
-Template taken from - https://www.twitterbio.io/.
+Template taken from - https://www.twitterbio.io/
 
 This project uses the [ChatGPT API](https://openai.com/api/) and the [Vercel AI SDK](https://sdk.vercel.ai/docs) with streaming. It constructs a prompt based on the form and user input, sends it to the ChatGPT API with a Vercel Edge Function, then streams the response back to the application UI.
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
     messages: [
       {
         role: 'user',
-        content: `You are a talk show host and you're about to interview a very famous startup founder. Based on the biography of this person, generate 2 ${vibe} potential interesting questions that a wide range of people might find interesting and clearly labeled "1." and "2."${
+        content: `You are a talk show host and you're about to interview someone. Based on the biography of this person:  ${bio}$ generate 2 ${vibe} potential interesting questions that a wide range of people might find interesting and clearly labeled "1." and "2."${
           vibe === 'Funny'
             ? "Make sure there is a joke in there and it's a little ridiculous."
             : null
