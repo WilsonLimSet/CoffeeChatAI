57905331c5986f48fa1de7b3cdb1c0f5fbb74001
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2025-01-01 15:10:13 -0800

    let scott test

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -1,36 +1,37 @@
 import { stripe } from "@/lib/stripe";
+import { NextResponse } from 'next/server';
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
     try {
         const body = await req.json();
         const { user_id, email, plan_name } = body;
 
         let session = await stripe.checkout.sessions.create({
-                customer_email: email,
-                line_items: [ 
-                    {
-                        price_data: {
-                            currency: 'usd',
-                            product_data: {
-                                name: plan_name
-                            },
-                            recurring: {
-                                interval: 'month'
-                            },
-                            unit_amount: 4 * 100,
+            customer_email: email,
+            line_items: [ 
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: plan_name
                         },
-                        quantity: 1,
+                        recurring: {
+                            interval: 'month'
+                        },
+                        unit_amount: 4 * 100,
                     },
-                ],
-                metadata: {
-                    user_id: user_id
+                    quantity: 1,
                 },
-                mode: 'subscription',
-                success_url: `https://www.coffeechatai.com/app`,
+            ],
+            metadata: {
+                user_id: user_id
+            },
+            mode: 'subscription',
+            success_url: `https://www.coffeechatai.com/app`,
         });
 
-        return Response.json({ paymentLink: session.url });
+        return NextResponse.json({ paymentLink: session.url });
     } catch (error) {
-        return Response.json({ error: error });
+        return NextResponse.json({ error: error }, { status: 500 });
     }
 }
