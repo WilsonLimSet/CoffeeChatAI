2cebee729bdeff66c0f2a56f8ed1072914b7236c
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2025-07-13 18:46:29 +0800

    fixes

--- app/api/cancel-subscription/route.ts
+++ app/api/cancel-subscription/route.ts
@@ -1,20 +1,30 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 import { stripe } from "@/lib/stripe";
-import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
 
-const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '', 
-  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
-);
-
 export async function POST(req: Request) {
+  const supabase = createRouteHandlerClient({ cookies }, {
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  });
+
   try {
+    const {
+      data: { user }
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+      return new NextResponse('User not found', { status: 404 });
+    }
+
     const { subscription_id } = await req.json();
+
     // Cancel the subscription on Stripe
     await stripe.subscriptions.cancel(subscription_id);
 
     // Update the Supabase row
-    const { error } = await supabaseAdmin
+    const { error } = await supabase
       .from('profiles')
       .update({
         paid: false,
